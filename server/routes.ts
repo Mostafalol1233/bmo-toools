300890ff110e02bbbfb7e804c5fb72dda8237092
Mostafalol1233 <1.99234098e+08+Mostafalol1233@users.noreply.github.com>
2026-08-17 18:42:46 +0000

    fix: make vercel api and static seo routes work

--- server/routes.ts
+++ server/routes.ts
@@ -1,9 +1,9 @@
 import type { Express } from "express";
 import { createServer, type Server } from "http";
-import { storage } from "./storage";
+import { storage } from "./storage.js";
 import { nanoid } from "nanoid";
-import { insertUrlSchema } from "@shared/schema";
-import { BLOG_SLUGS, SEO_BASE_URL, TOOL_SEO } from "@shared/seo";
+import { insertUrlSchema } from "../shared/schema.js";
+import { BLOG_SLUGS, SEO_BASE_URL, TOOL_SEO } from "../shared/seo.js";
 import FormData from "form-data";
 import axios from "axios";
 
